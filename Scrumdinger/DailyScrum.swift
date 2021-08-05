6a8f9f1306230d9bc39d2b4da177ba1211853be4
Anagha <anaghajayaprakash1996@gmail.com>
2021-08-06 00:02:27 +0530

    feat: Create MeetingView

---
+++ Scrumdinger/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Anagha K J on 05/08/21.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Stark", "Rogers", "Odinson"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Banner", "Romonoff", "Barton"], lengthInMinutes: 20, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["Danvers", "Barnes", "Maximoff"], lengthInMinutes: 15, color: Color("Web Dev")),
+        ]
+    }
+}
