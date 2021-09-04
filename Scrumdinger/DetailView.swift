fea42af9def3a77ad6bddd3a243a1fb7e16e3672
Anagha <anaghajayaprakash1996@gmail.com>
2021-09-05 00:51:01 +0530

    feat: Set up navigation items

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresented = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -38,6 +39,20 @@ struct DetailView: View {
         }
         .listStyle(InsetGroupedListStyle())
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView()
+                    .navigationTitle(Text(scrum.title))
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    },trailing: Button("Done") {
+                        isPresented = false
+                    })
+            }
+        }
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
     }
 }
 
