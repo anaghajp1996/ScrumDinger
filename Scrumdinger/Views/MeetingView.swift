9ac3554d029266230a97460159e280cfb46a8d6e
Anagha <anaghajayaprakash1996@gmail.com>
2022-06-22 14:45:54 -0500

    feat: Extract meeting header

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -14,20 +14,6 @@ struct MeetingView: View {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5, total: 15)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds elapsed").font(.caption)
-                        Label("300", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds remaining").font(.caption)
-                        Label("600", systemImage: "hourglass.tophalf.fill")
-                    }
-                }.accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time Remaining")
-                .accessibilityValue("10 Minutes")
                 Circle().strokeBorder(lineWidth: 24, antialiased: true)
                 HStack {
                     Text("Speaker 1 of 3")
